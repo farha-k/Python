a6ba134596742c7130d4d0fe18b59b9ec7e48181
farha-k <farahaklm2005@gmail.com>
2025-07-28 15:29:06 +0400

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 21 18:19:40 2025
+
+@author: farha
+"""
+
+rows=5
+for i in range(1, rows + 1 ):
+   print(('*' * (2 * i - 1)).center(2 * rows - 1))
+   
+   
+   
